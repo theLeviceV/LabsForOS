c8f7d0d3456da0d34376f27aafe68d7f225a3afb
Levice V <81415955+theLeviceV@users.noreply.github.com>
2023-12-24 14:59:54 +0700

    Update a.sh

--- 1/scripts/a.sh
+++ 1/scripts/a.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-echo worked!
+
+# Check if exactly two parameters are passed
+if [ "$#" -ne 2 ]; then
+    echo "You need to provide exactly two parameters."
+    exit 1
+fi
+
+# Print the name of the script, the value of both parameters, as well as a message about the equality or inequality of the transmitted strings
+echo "Script Name: $0"
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
+
+if [ "$1" == "$2" ]; then
+    echo "The transmitted strings are equal."
+else
+    echo "The transmitted strings are not equal."
+fi
+
+# Repeat the script with the output of information to the file outa.
+# If the file already exists, it will be overwritten.
+# If it doesn't exist, it will be created.
+echo "Script Name: $0" > outa
+echo "Parameter 1: $1" >> outa
+echo "Parameter 2: $2" >> outa
+
+if [ "$1" == "$2" ]; then
+    echo "The transmitted strings are equal." >> outa
+else
+    echo "The transmitted strings are not equal." >> outa
+fi
