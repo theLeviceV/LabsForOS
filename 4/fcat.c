03e080b448cd3cd4e3f66a0378eb76a6417a939e
theLeviceV <levice@debian.debianForLabs>
2023-12-25 17:44:50 +1000

    Update Lab4

--- 4/fcat.c
+++ 4/fcat.c
@@ -1,79 +1,60 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <getopt.h>
 
-int read_file(const char *file, int lines, int line_number);
-void print_lines(int fd, int lines, int line_number);
+int readfile(char *filename, char find, int start) {
+    FILE *file;
+    int count = 0;
+    char line[1024];
 
-int main(int argc, char *argv[]) {
-    int lines = 0;
-    int line_number = 1;
-
-    // check for options
-    if (argc > 1 && argv[1][0] == '-') {
-        lines = atoi(&argv[1][2]);
-        argv++;
-        argc--;
-    }
-
-    if (argc == 1) {
-        printf("Usage: %s [-n<integer>] <file list>\n", argv[0]);
-        exit(1);
-    }
-
-    for (int i = 1; i < argc; i++) {
-        if (read_file(argv[i], lines, line_number) == -1) {
-            perror("Error reading file");
-            exit(1);
-        }
-    }
-
-    return 0;
-}
-
-int read_file(const char *file, int lines, int line_number) {
-    struct stat sb;
-    if (stat(file, &sb) == -1) {
-        perror("Cannot stat file");
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
         return -1;
     }
 
-    if (!S_ISREG(sb.st_mode)) {
-        fprintf(stderr, "%s is not a regular file\n", file);
-        return -1;
-    }
-
-    int fd = open(file, O_RDONLY);
-    if (fd == -1) {
-        perror("Cannot open file");
-        return -1;
-    }
-
-    print_lines(fd, lines, line_number);
-
-    close(fd);
-    return 0;
-}
-
-void print_lines(int fd, int lines, int line_number) {
-    char buffer[BUFSIZ];
-    ssize_t bytes_read;
-    int chars_printed = 0;
-
-    while ((bytes_read = read(fd, buffer, BUFSIZ)) > 0) {
-        for (ssize_t i = 0; i < bytes_read; i++) {
-            if (buffer[i] == '\n') {
-                line_number++;
-                chars_printed = 0;
-            } else {
-                if (lines == 0 || chars_printed < lines) {
-                    putchar(buffer[i]);
-                    chars_printed++;
+    while (fgets(line, sizeof(line), file)) {
+        count++;
+        if (count >= start) {
+            if (find) {
+                if (strchr(line, find)) {
+                    printf("%s", line);
                 }
+            } else {
+                printf("%s", line);
             }
         }
     }
+
+    fclose(file);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    int opt, start = 1;
+    char find = 0;
+
+    while ((opt = getopt(argc, argv, "f:")) != -1) {
+        switch (opt) {
+            case 'f':
+                find = *optarg;
+                break;
+            default:
+                printf("Usage: %s [-f<character>] <file list>\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        printf("Expected file list.\n");
+        return 1;
+    }
+
+    for (; optind < argc; optind++) {
+        readfile(argv[optind], find, start);
+        start = 1;
+    }
+
+    return 0;
 }
